88daa64b91320cc5092e3f3c3e7c4404b569f053
CARIT\jouri.dejong <j.dejong@audev.com>
2021-05-10 21:11:13 +0200

    Phase one of using modules?

--- src/Collider.ts
+++ src/Collider.ts
@@ -1,42 +1,47 @@
-class Collider
+module Colliders
 {
-    constructor()
+    import * as Graphics from './Graphics';
+
+    abstract class Collider
     {
+        constructor() { }
 
-    }
-
-    public CheckCollision(a: Collider): boolean
-    {
-        return false;
-    }
-}
-
-class BoxCollider extends Collider
-{
-    Posistion: Point;
-    Width: number;
-    Height: number;
-    
-    constructor(Width: number, Height: number, Position: Point)
-    {
-        super();
-
-        this.Width = Width;
-        this.Height = Height;
-        this.Posistion = Position;
-
-        //console.log("Collider created");
-    }
-    
-    public CheckCollision(a: BoxCollider): boolean
-    {
-        if(this.Posistion.x < a.Posistion.x + a.Width && this.Posistion.x + this.Width > a.Posistion.x && this.Posistion.y < a.Posistion.y + a.Height && this.Posistion.y + this.Height > a.Posistion.y)
-        {
-            return true;
-        }
-        else
+        public CheckCollision(a: Collider): boolean
         {
             return false;
+        }
+    }
+
+    export class BoxCollider extends Collider
+    {
+        Posistion: Point;
+        Width: number;
+        Height: number;
+        Rect: Graphics.Rect;
+        
+        constructor(Width: number, Height: number, Position: Point)
+        {
+            super();
+
+            this.Width = Width;
+            this.Height = Height;
+            this.Posistion = Position;
+
+            this.Rect = new Rect(Width, Height, "green");
+
+            //console.log("Collider created");
+        }
+        
+        public CheckCollision(a: BoxCollider): boolean
+        {
+            if(this.Posistion.x < a.Posistion.x + a.Width && this.Posistion.x + this.Width > a.Posistion.x && this.Posistion.y < a.Posistion.y + a.Height && this.Posistion.y + this.Height > a.Posistion.y)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
